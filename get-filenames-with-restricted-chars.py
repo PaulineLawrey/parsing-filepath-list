0c8b58805cc50fb265d8e236c327597d1939abd8
Pauline Lawrey <pauline.lawrey@jcu.edu.au>
2023-08-02 10:41:44 +1000

    fixed error in invalid character list

--- get-filenames-with-restricted-chars.py
+++ get-filenames-with-restricted-chars.py
@@ -1,11 +1,11 @@
 import csv
 import re
 
-INPUTFILE = 'mjs-filepaths.csv'
-OUTPUTFILE = 'invalid-OneDrive-filenames.csv'
+INPUTFILE = './data/mjs-filepaths.csv'
+OUTPUTFILE = './data/invalid-OneDrive-filenames.csv'
 
 def find_special_characters(filename, output_filename):
-    special_chars = r'["*:<>?/\\|]'  # Regex pattern for special characters
+    special_chars = r'["*:<>?\|]'  # Regex pattern for special characters
     with open(filename, 'r') as input_csv, open(output_filename, 'w', newline='') as output_csv:
         reader = csv.reader(input_csv)
         writer = csv.writer(output_csv)
